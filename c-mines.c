24801a888fce4b2a29b604829684e6c1fc4e3cf9
dcodeh <dcodyburrows@gmail.com>
2017-09-25 14:28:15 -0400

    Finished documentation
    Hopefully it all makes sense, and doesn't have any typos.
    
    A makefile is on the way sometime.....

--- c-mines.c
+++ c-mines.c
@@ -1,6 +1,22 @@
 // c-mines.c
-// @author Cody Burrows
-// /////////////////////////////////////////
+// @author Cody Burrows <dcodyburrows@gmail.com>
+// 
+// This is an minesweeper-like game made using c and ncurses.
+// usage: ./mines len num
+// where len and num are integers, and len specifies the side
+// length of the lenxlen grid. num specifies the number of
+// mines to plant on the grid.
+//
+// Use wasd to move the cursor, e to mark a mine, and r to 
+// uncover a space -- use with caution!
+//
+// Board Legend:
+// #   = covered
+// *   = mine
+// 0   = empty
+// 1-8 = number of adjacent mines
+//
+// //////////////////////////////////////////////////////////
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -9,46 +25,68 @@
 #include <stdbool.h>
 #include "c-mines.h"
 
+/**
+  * This function parses cmd line args, and initializes the game
+  *
+  * @param argc     The number of arguments passed
+  * @param argv     The arguments...see above for usage
+  *
+  * @returns EXIT_SUCCESS most of the time, unless you do something
+  *          you shouldn't have.
+  */
 int main(int argc, char ** argv) {
 
+    // correct number of args
     if(argc == 3) {
         
         int size, num;
+
+        // convert args to base-10 numbers
         size = strtol(argv[1], NULL, 10);
         num = strtol(argv[2], NULL, 10);
 
         if(size <= 0 || num <= 0) {
+            // you don messed this one up bruh
             print_usage();
             return EXIT_FAILURE;
         } else if(size >= 1024) {
+            // sorry, too big
             size = 1024;
         } else if(num >= (size * size)) {
+            // sorry, too many
             num = (size * size);
         }
-/*
-        printf("================C-MINES================\n");
-        printf("    == Board Size: %dx%d\n", size, size);
-        printf("    == Number of mines: %d\n", num);
-        printf("    == Difficulty: %d\n", (10 * num) / (size * size));
-        printf("=======================================\n");
-*/
+
+        // holds the values for each cell
+        // value    /   meaning
+        // 0        /   empty
+        // 1        /   1 mine touching this square
+        // 2        /   2 mines touching this square
+        // ...      /   ...
+        // 8        /   8 mines touching this square
+        // *        /   bomb here 
         char ** board = create_board(size, '0');
         init_board(board, size, num);
 
+        // stores the grid that the user sees
         char ** view = create_board(size, '#');
 
+        // woo hoo
         play_game(board, view, size, num);
 
+        // be a good process...
         destroy_board(view, size);
         destroy_board(board, size);
         return EXIT_SUCCESS;
         
     } else {
+        // not enough args bruh
         print_usage();
         return EXIT_FAILURE;
     }
 }
 
+/// How do I use this dumb thing again?
 void print_usage(void) {
 
     fprintf(stderr, "Usage: mines len num\n");
@@ -57,7 +95,7 @@ void print_usage(void) {
     
 }
 
-// just allocates a board of specified size!
+/// Allocates a square array of specified size, filled with a char
 char ** create_board(int size, char fill) {
 
     char ** board = malloc(size * sizeof(char *));
@@ -67,6 +105,7 @@ char ** create_board(int size, char fill) {
         board[i] = malloc(size * sizeof(char));
         for(int j = 0; j < size; ++j) {
 
+            // handy
             board[i][j] = fill;
 
         }
@@ -77,19 +116,14 @@ char ** create_board(int size, char fill) {
 
 }
 
-/*
- * Legend:
- * #   = covered
- * *   = mine
- * 0   = empty
- * 1-8 = number of adjacent mines
- */
-void init_board(char ** board, int size, int num) {
 
+/// places a number of mines on the board, then calculates adjacent cells
+void init_board(char ** board, int size, int num) {
 
     int count = 0;
     int r, c;
 
+    // for super random mode plus use '48' as the seed
     srand(time(0));
     while(count < num) {
         r = rand() % size;
@@ -101,50 +135,54 @@ void init_board(char ** board, int size, int num) {
         }
     }
 
+    // don't do the work but take credit for it
     calculate_cells(board, size);
 
 }
 
+/// Be a good process...
 void destroy_board(char ** board, int size) {
 
     for(int i = 0; i < size; ++i) {
 
+        // read as free bird
         free(board[i]);
 
     }
 
+    // see comment above
     free(board);
 
 }
 
-void print_board(char ** board, int size) {
-    for(int r = 0; r < size; ++r) {
-        for(int c = 0; c < size; ++c) {
-            printf(" %c ", board[r][c]);
-        }
-
-        printf("\n");
-    }
-}
-
+/// Calculates the number of mines adjacent to each cell
 void calculate_cells(char ** board, int size) {
 
+    // whoa that's a lot of loops
+    
+    // go through each cell in the board
     for(int r = 0; r < size; ++r) {
         for(int c = 0; c < size; ++c) {
 
+            // if it's not a mine, check for mines around it
             if(board[r][c] != '*') {
                 int total = 0;
+
+                // take a look at adjacent cells
                 for(int sr = r - 1; sr <= r + 1; ++sr) {
                     for(int sc = c - 1; sc <= c + 1; ++sc) {
 
                         // make sure they're safe to dereference
                         if((sr >= 0 && sc >= 0) && (sr < size) && (sc < size)) {
+
+                            // bingo!
                             if(board[sr][sc] == '*')
                                 total++;
                         }
                     }
                 }
 
+                // put the number in as a char
                 board[r][c] = total + '0';
 
             }
@@ -153,9 +191,12 @@ void calculate_cells(char ** board, int size) {
     }
 }
 
+/// It's simple. Just do everything.
 int play_game(char ** board, char ** view, int size, int num) {
     
     time_t start = time(NULL);
+
+    // ncurses init stuff
     initscr();
     raw();
     noecho();
@@ -168,23 +209,28 @@ int play_game(char ** board, char ** view, int size, int num) {
     bool dead = false;
     int ch;
     
+    // while you're playing basically
     while(run) {
         
-
         show_board(view, size, r, c);
 
+        // get your keystrokes from ncurses
         ch = getch();
         if(ch == 'q') {
             // i give up
             run = false;
             dead = true;
         } else if(ch == 'w') {
+            // move up
             r--;
         } else if(ch == 's') {
+            // move down
             r++;
         } else if(ch == 'a') {
+            // move left
             c--;
         } else if(ch == 'd') {
+            // move right
             c++; // lol
         } else if(ch == 'e') {
             // mark the space
@@ -205,20 +251,25 @@ int play_game(char ** board, char ** view, int size, int num) {
             // detonate
             switch(uncover(board[r][c])) {
                 case 0:
+                    // phew. still alive.
                     view[r][c] = board[r][c];
                     break;
 
                 case 1:
                     view[r][c] = '*';
-                    // you lose!
+                    // oh, that's a bomb
+                    // *explodes*
                     run = false;
                     dead = true;
                     break;
 
                 case -1:
+                    // thank God, it's just dirt
                     view[r][c] = '.';
                     Cell * head = NULL;
                     Cell ** handle = &head;
+
+                    // auto-clear the rest of the dirt 
                     clear_area(r, c, view, board, handle, size);
                     destroy_cells(handle);
                     break;
@@ -226,15 +277,19 @@ int play_game(char ** board, char ** view, int size, int num) {
             
         }
 
+        // wrap around the top/bottom
         if(r < 0)
             r = size - 1;
 
+        // wrap around the edges
         if(c < 0)
             c = size - 1;
 
+        // more wrapping
         r = r % size;
         c = c % size;
 
+        // this is pretty lazy, but it works
         if(mines_marked == num) {
             // win!
             run = false;
@@ -243,6 +298,7 @@ int play_game(char ** board, char ** view, int size, int num) {
 
     }
 
+    // end, denit ncurses
     time_t end = time(NULL);
     refresh();
     curs_set(1);
@@ -250,19 +306,23 @@ int play_game(char ** board, char ** view, int size, int num) {
 
     // game over...did you win?
     if(dead) {
+        // guess not
         printf("\n\nYou died in a glorious ball of fire after only"
                 " %ld seconds.\n\n", (long) end - (long) start);
         print_summary(num, mines_marked, size);
     } else {
+        // good job
         printf("\n\nNice work! You found all of the mines in %ld seconds. \n\n",
                 (long) end - (long) start);
         print_summary(num, mines_marked, size);
     }
 
+    // return total time
     return (long) end - (long) start;
 
 }
 
+/// draws the board, and prints the cursor. duh
 void show_board(char ** view, int size, int cursr, int cursc) {
     
     move(0, 0);
@@ -287,12 +347,14 @@ void show_board(char ** view, int size, int cursr, int cursc) {
 
 }
 
-/*
- * Returns -1 if empty space (clearing required)
- * Returns  0 if safe, no clearing required
- * Returns  1 if bomb...you lose
- */
+/// decides your fate
 int uncover(char board) {
+
+    /*
+     * Returns -1 if empty space (clearing required)
+     * Returns  0 if safe, no clearing required
+     * Returns  1 if bomb...you lose
+     */
 
     if(board == '*') {
         return 1;
@@ -304,6 +366,7 @@ int uncover(char board) {
 
 }
 
+/// a nicely formatted summary
 void print_summary(int num, int right, int size) {
     printf("\n=================== Mines Summary ===================\n");
     printf("Board Size: %dx%d\n", size, size);
@@ -314,20 +377,25 @@ void print_summary(int num, int right, int size) {
 }
 
 // warning: big honkin' recursive function ahead
+
+/// clear empty spaces
 void clear_area(int r, int c, char ** view, char ** board, Cell ** head, int size) {
 
+    // check the neighbors for empty spaces
     for(int sr = r - 1; sr <= r + 1; ++sr) {
         for(int sc = c - 1; sc <= c + 1; ++sc) {
 
             // make sure they're safe to dereference
             if((sr >= 0 && sc >= 0) && (sr < size) && (sc < size)) {
 
+                // found an empty cell, recurse on it
                 if(board[sr][sc] == '0') {
                     if(add_cell(head, sr, sc)) {
                         clear_area(sr, sc, view, board, head, size);
                         view[sr][sc] = '.';
                     }
                 } else {
+                    // found a number. print it
                     view[sr][sc] = board[sr][sc];
                 }
             }
@@ -336,14 +404,17 @@ void clear_area(int r, int c, char ** view, char ** board, Cell ** head, int siz
 
 }
 
+/// insert a cell into the unique list
 bool add_cell(Cell ** handle, int r, int c) {
     
     Cell * head = *handle; // will be changed
     Cell * front = *handle; // want to keep record of
 
+    // check for uniqueness
     while(head) {
         
         if(head->r == r && head->c == c) {
+            // already there
             return false;
         }        
 
@@ -357,10 +428,12 @@ bool add_cell(Cell ** handle, int r, int c) {
     (*handle)->r = r;
     (*handle)->c = c;
 
+    // wasn't there before, but it is now
     return true;
 
 }
 
+/// teach that sucker a lesson, and be a good process...
 void destroy_cells(Cell ** handle) {
 
     Cell * node = *handle;
--- c-mines.h
+++ c-mines.h
@@ -1,38 +1,175 @@
 // c-mines.h
 // @author Cody Burrows
-// ///////////////////////////////////////
+//
+// This file contains function and struct definitions
+// for c-mines.c
+// //////////////////////////////////////////////////
 
-#ifndef CMINES
-#define CMINES
+// include guard
+#ifndef CMINES_H
+#define CMINES_H
 
+/**
+  * This struct is used to store data for a cell.
+  * It is used for clearing large empty areas.
+  */ 
 typedef struct cells {
     int r, c;
+    
+    // it's a linked list because I'm pretty lazy
     struct cells * next;
 } Cell;
 
+/**
+  * This function is used for clearing area.
+  * Its purpose is to insert a cell at the head of a list of 
+  * unique cells. A cells are equal if their row and column 
+  * are the same.
+  *
+  * @param  handle      A pointer to the head of the list
+  * @param  r           The row
+  * @param  c           The column
+  *
+  * @returns true if the row/column pair passed in is unique,
+  *               and was successfully added.
+  *          false if the row/column pair was already in the list.
+  */ 
 bool add_cell(Cell ** handle, int r, int c);
 
+/**
+  * This function cleans up the linked cell list. 
+  *
+  * @param  handle      A pointer to the head of the list
+  * @returns nothing
+  */ 
 void destroy_cells(Cell ** handle);
 
+/**
+  * This handy little function prints out the correct usage for
+  * command line arguments.
+  *
+  * @param  void
+  *
+  * @returns nothing
+  */
 void print_usage(void);
 
+/**
+  * This function places mines on the board. It will
+  * ensure that exactly num mines are placed, if there is room.
+  *
+  * @param board    The board to place mines on
+  * @param size     The dimension of the square board
+  * @param num      The number of mines to place
+  */
 void init_board(char ** board, int size, int num);
 
+/**
+  * This function initializes an empty board (2d char array) with
+  * a specified character.
+  *
+  * @param size     The dimension of the square board. E.G. 3 would
+  *                 create a 3x3 board consisting of 9 spaces.
+  * @param fill     The character to initialize each element with.
+  *
+  * @returns A pointer to a beautiful pre-fab 2d char array
+  */
 char ** create_board(int size, char fill);
 
+/**
+  * Obliterate that sucker.
+  *
+  * @param  board       The 2d char array to destroy
+  * @param  size        The dimension of the square board
+  */ 
 void destroy_board(char ** board, int size);
 
-void print_board(char ** board, int size);
-
+/**
+  * Calculates the number of mines touching each cell in the board,
+  * and updates the values.
+  *
+  * The board passed in should be the game board that contains values,
+  * not the one the player sees.
+  *
+  * @param  board       The board to calculate and update
+  * @param  size        The dimension of the square board
+  * 
+  * @returns nothing    The board is modified in place
+  */
 void calculate_cells(char ** board, int size);
 
+/**
+  * The juicy part that handles playing the game.
+  * 
+  * This involves:
+  *     - Keeping track of the time
+  *     - Init/denit of ncurses mode
+  *     - Handling user input (from ncurses)
+  *     - Handling marking mines, uncovering spaces, clearing area,
+  *       and uncovering a bomb.
+  *
+  * @param board    The 2d char array that stores mine locations, and the
+  *                 values of surrounding cells
+  * @param view     The 2d char array that holds the board the user sees
+  * @param size     The dimension of board and view
+  * @param num      The number of mines on the board
+  * 
+  * @return the total number of seconds the game was played.
+  */
 int play_game(char ** board, char ** view, int size, int num);
 
+/**
+  * Uses ncurses to display an information banner, and the grid that
+  * the player sees. This function also draws the cursor around the
+  * active cell.
+  *
+  * @param view     The board to display on the screen
+  * @param size     The dimension of the square board
+  * @param cursr    The row that the cursor is on
+  * @param cursc    The col that the cursor is on
+  *
+  * @returns nothing    The board is not modified in any way
+  */ 
 void show_board(char ** view, int size, int cursr, int cursc);
 
+/**
+  * This function determines whether or not you die. Don't make it angry.
+  *
+  * @param board    The char to test (where the user hit r)
+  * @returns 1 if the character is a bomb ('*')
+  *         -1 if the character is a space ('0')
+  *          0 if the character is anything else
+  */
 int uncover(char board);
 
+/**
+  * Shows some stats, some of which will be useful to a coroner, such as
+  * Time of Death, Cause of Death, etc.
+  *
+  * ...and also board size, number of mines, difficulty, 
+  * and the number of bombs you marked right.
+  *
+  * @param num      The number of bombs on the board
+  * @param right    The number of mines you correctly guessed the location of
+  * @param size     The dimension of the square board
+  *
+  * @return nothing it just prints things
+  */
 void print_summary(int num, int right, int size);
 
+/**
+  * This beast clears open areas of the field for you, so you can
+  * get on with exploding faster!
+  *
+  * If you uncover an empty space, it will automatically uncover empty
+  * spaces nearby, up to any numbered cells.
+  *
+  * @param r        The row the user clicked on
+  * @param c        The col the user clicked on
+  * @param view     The board the user sees
+  * @param head     A pointer to a unique list of cells
+  * @param size     The dimension of the square board
+  */
 void clear_area(int r, int c, char ** view, char ** board, Cell ** head, int size);
-#endif
+
+#endif // end of include guard
